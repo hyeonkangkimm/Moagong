f2b626d3958d8c8ac98d7cb0a38c67bf654f36d7
hyeonkangkimm <kkhk75@gmail.com>
2026-03-18 12:41:45 +0900

    Feat: chat controller 수정

--- src/main/java/com/example/skunivProject/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/skunivProject/domain/chat/controller/ChatController.java
@@ -22,6 +22,7 @@ public class ChatController {
     private final ChatRoomService chatRoomService;
     private final ChatMessageService chatMessageService;
 
+    
     /**
      * 팀 채팅방 생성
      */
